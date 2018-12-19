5f5302e8cc8ecf53fae3fe6ac7f8fb094dc6854b
giruzou <udon.yuto@gmail.com>
2018-12-19 19:25:57 +0900

    Initial Commit

---
+++ kakaokakaogameTests/kakaokakaogameTests.swift
@@ -0,0 +1,34 @@
+//
+//  kakaokakaogameTests.swift
+//  kakaokakaogameTests
+//
+//  Created by 久々湊優人 on 2018/12/19.
+//  Copyright © 2018 atzou. All rights reserved.
+//
+
+import XCTest
+@testable import kakaokakaogame
+
+class kakaokakaogameTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
